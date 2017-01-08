7ef623b7cd6983876de5946bc5d3713e06a7db70
Nhan Tran <nhantrantnt@gmail.com>
2017-01-08 02:24:05 -0700

    added car detection and fixed human detection

--- 00-projects/car_detection.py
+++ 00-projects/car_detection.py
@@ -1,33 +1,33 @@
 import cv2
+import time
 import numpy as np
 
 # Create our body classifier
-source_input = "/Users/nhant/Google Drive/OnlineLearning/selflearning_CV_with_Python/practice_sources/images/"
-body_classifier = cv2.CascadeClassifier('../06_face_ppl__car_detection/haarcascade_fullbody.xml')
+car_classifier = cv2.CascadeClassifier('../06_face_ppl__car_detection/haarcascade_car.xml')
 
 # Initiate video capture for video file
-cap = cv2.VideoCapture(source_input + 'walking.avi')
+source_input = "/Users/nhant/Google Drive/OnlineLearning/selflearning_CV_with_Python/practice_sources/images/"
+cap = cv2.VideoCapture(source_input+'cars.avi')
 
 # Loop once video is successfully loaded
 while cap.isOpened():
 
+    time.sleep(.05)
     # Read first frame
     ret, frame = cap.read()
+
     # prevent Error:  empty Mat to grayscale after last frame, so exit the loop if frame is empty.
     if frame is None:
         break
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
-
-
-
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Pass frame to our body classifier
-    bodies = body_classifier.detectMultiScale(gray, 1.2, 3)
+
+    # Pass frame to our car classifier
+    cars = car_classifier.detectMultiScale(gray, 1.4, 2)
 
     # Extract bounding boxes for any bodies identified
-    for (x, y, w, h) in bodies:
+    for (x, y, w, h) in cars:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
-        cv2.imshow('Pedestrians Detection', frame)
+        cv2.imshow('Cars', frame)
 
     if cv2.waitKey(1) == 13:  # 13 is the Enter Key
         break
